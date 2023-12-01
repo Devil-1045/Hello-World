43943a8ca6fdc490c2ae85f25040dfa1f15a6a2f
Devil-1045 <141399788+Devil-1045@users.noreply.github.com>
2023-12-01 18:38:41 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,13 @@
 # Python Programming
+def out_square(a, b):
+    sq1=a**2
+    sq2=b**2
+
+    def in_add(a, b):
+        return a + b
+    
+    print("square of", a,"is", sq1)
+    print("square of", b,"is", sq2)
+    print("addition of", a,"and", b,"is",in_add(a, b))  
+
+out_square(5, 10)
